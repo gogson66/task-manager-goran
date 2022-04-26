05c6e625670512ed85babb8f3317e7d1178f20ab
gogson66 <cvijanovicgoran78@gmail.com>
2022-04-26 11:38:11 +0200

    removing node_modules

--- playground/playground-mongoose.js
+++
@@ -1,4 +0,0 @@
-require('../src/db/mongoose')
-const Task = require('../src/models/task')
-
-Task.findByIdAndDelete('622f5707aaba7fb95da40d4d').then(() => Task.countDocuments({completed: true})).then(count => console.log(count)).catch(e => console.log(e))
